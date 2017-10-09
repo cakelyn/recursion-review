fac93deab28ea241f0c4d2a95b833a31c9926689
Katelyn Stamas <cakelyn@gmail.com>
2017-10-09 15:35:24 -0400

    Set up Floobits, fixed the test suite for getElementsByClassName and did most of the alorithm. Ended at object testing.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === true) {
+    return 'true';
+  }
+  if (obj === false) {
+    return 'false';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr.join(',') + ']';
+  }
+  if (typeof obj === 'object') {
+    var tempObj = [];
+    for (var key in obj) {
+      tempObj.push( stringifyJSON(obj[key]) );
+    }
+    return '{}';
+  }
+
 };
